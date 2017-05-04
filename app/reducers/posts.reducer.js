7c42f37a3076a3deae7ee191f7dad7f54440ed53
VAdims Samsinovs <vsamsinovs@gmail.com>
2017-05-04 10:51:47 +0100

    bug fixing

--- app/reducers/posts.reducer.js
+++ app/reducers/posts.reducer.js
@@ -5,39 +5,39 @@ import { browserHistory } from 'react-router';
 import _ from 'lodash';
 
 export default function postsReducer(state = initialState.posts, action) {
-    let posts = [];
-    switch (action.type) {
-        case types.LOAD_POSTS_SUCCESS:
-            return _.sortBy(action.posts, post => {
-                return -post.upvotes
-            });
-        case types.UPDATE_POST_SUCCESS:
-            browserHistory.push(`/posts/${action.post._id}`);
-            posts = [
-                ...state.filter(post => post._id !== action.post._id),
-                Object.assign({}, action.post)
-            ];
-            return _.sortBy(posts, post => {
-                return -post.upvotes
-            });
-        case types.CREATE_POST_SUCCESS:
+  let posts = [];
+  switch (action.type) {
+    case types.LOAD_POSTS_SUCCESS:
+      return _.sortBy(action.posts, post => {
+        return -post.upvotes
+      });
+    case types.UPDATE_POST_SUCCESS:
+      browserHistory.push(`/posts/${action.post._id}`);
+      posts = [
+        ...state.filter(post => post._id !== action.post._id),
+        Object.assign({}, action.post)
+      ];
+      return _.sortBy(posts, post => {
+        return -post.upvotes
+      });
+    case types.CREATE_POST_SUCCESS:
 
-            browserHistory.push(`/posts/${action.post._id}`);
-            posts = [
-                ...state.filter(post => post._id !== action.post._id), Object.assign({}, action.post)
-            ];
-            return _.sortBy(posts, post => {
-                return -post.upvotes
-            });
-        case types.DELETE_POST_SUCCESS:
-            browserHistory.push(`/posts/`);
-            posts = [
-                ...state.filter(post => post._id !== action.postId)
-            ];
-            return _.sortBy(posts, post => {
-                return -post.upvotes
-            });
-        default:
-            return state;
-    }
+      browserHistory.push(`/posts/${action.post._id}`);
+      posts = [
+        ...state.filter(post => post._id !== action.post._id), Object.assign({}, action.post)
+      ];
+      return _.sortBy(posts, post => {
+        return -post.upvotes
+      });
+    case types.DELETE_POST_SUCCESS:
+      browserHistory.push(`/posts/`);
+      posts = [
+        ...state.filter(post => post._id !== action.postId)
+      ];
+      return _.sortBy(posts, post => {
+        return -post.upvotes
+      });
+    default:
+      return state;
+  }
 }
